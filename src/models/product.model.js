69bd2b6a7c182ca7078125eac9196c8e2a66d1d9
Gopala <guptaraja727@gmail.com>
2022-01-05 22:58:25 +0530

    ccc

---
+++ src/models/product.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    email: { type: String, required: true },
+    pin_code: { type: Number, required: true },
+    age: { type: Number, required: true },
+    gender: { type: String, required: true },
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("product", productSchema);
